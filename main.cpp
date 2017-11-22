904c4b3290a97933bbb79a63d66deebc6189a3c4
wschillingtest <walter.schilling@computer.org>
2017-11-22 16:36:21 -0600

    Added source code to initial repository.

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include "judge.h"
+
+using namespace std;
+
+void menu();
+
+int main(){
+  int option, type;
+  string name;
+  Judge controller;
+  while(true){
+    menu();
+    cin >> option;
+    system("clear");
+    switch(option){
+      case 1:
+        // nowa gra
+		cout << "1. Player vs. Player \n";
+        cout << "2. Player vs. Computer \n";        
+		cin >> type;
+        system("clear");
+        controller.newGame(type);
+	      break;
+      case 2:
+        // wczytaj grę
+        controller.loadGame("last.sav");
+        break;
+      case 3:
+        // statystyki
+        controller.stats();
+        break;
+      case 4: {
+          ifstream manual("manual.txt");
+          string line;
+          while(getline(manual, line)) cout << line << '\n';
+          cout << "\n\nPress ENTER to continue...";
+          cin.get();
+          cin.get();
+          system("clear");
+        }
+        break;
+      case 5:
+        return 0;
+	      break;
+      default:
+        cout << "There is no such option! \n";
+        continue;
+    }
+  }
+  return 0;
+}
+
+void menu(){
+cout << "======== SHIPS ======= \n";
+  cout << "1. New game \n";
+  cout << "2. Load the game \n";
+  cout << "3. Statistics \n";
+  cout << "4. Manual \n";
+  cout << "5. Finish \n";
+}
---
+++ player.cpp
@@ -0,0 +1,69 @@
+#include "player.h"
+#include <cstdlib>
+#include <ctime>
+
+// zapisywanie danych gracza do strumienia
+ostream & operator<<(ostream & os, const Player & p){
+  os << p.name << ' ' << p.moves <<  ' ' <<
+    p.hits << ' ' << p.misses << ' ' << p.ships << '\n';
+  os << p.my << p.enemy;
+  return os;
+}
+
+// wczytywanie danych gracza ze strumienia
+istream & operator>>(istream & is, Player & p){
+  is >> p.name >> p.moves >> p.hits >> p.misses >> p.ships;
+  is >> p.my >> p.enemy;
+  return is;
+}
+
+// ruch czlowieka
+int HumanPlayer :: move () {
+  int x, y;
+  // show the player info
+  cout << "Player's movement" + name << '\n';
+  cout << "Hit:" << hits << ", losses =" << 20 - ships << "\n\n";
+  cout << "Your ships: \n\n";
+  cout << my << '\n';
+  cout << "\n\nYour moves: \n\n";
+  cout << enemy << '\n';
+  // Ask for coherent fields
+  do {
+    cout << "Please enter the following (x y):";
+    if (! (cin >> x >> y)) {// give a different character than the number
+      cin.clear (); // we clean the stream
+      cin.ignore (); // ignore the rest of the characters
+      return -1; // information about game break
+    }
+    // Repeat this until the player enters the correct field
+  } while (! (x <10 && x>= 0 && y <10 && y>= 0) || enemy.get (x * 10 + y) == '0' || enemy.get (x * 10 + y) == 'X');
+  ++ moves; // increase the number of moves
+  cout << '\n';
+  return x * 10 + y; // return the field number
+}
+
+// ruch gracza komputerowego
+int ComputerPlayer::move(){
+  srand(time(0)); // nowe ziarno losowosci
+  int x;
+  std::cout << enemy << '\n';
+  do {
+    x = rand() % 100;
+    // losuj az trafi na niesprawdzone pole
+  } while(enemy.get(x) == '0' || enemy.get(x) == 'X');
+
+  ++moves; // zwieksz liczbe ruchow
+  return x; // zwroc numer pola
+}
+
+// sprawdzanie pola przeciwnika
+bool Player::check(int t) {
+  if(my.get(t) == '#'){ // jesli trafiono statek
+    my.set(t, 'X'); // oznaczamy 'trafienie'
+    --ships; // zmniejsz liczbe statkow
+    return true; // true - trafiony
+  } else {
+    my.set(t, '0'); // oznaczamy 'pudlo'
+  }
+  return false; // false - pudlo
+}
